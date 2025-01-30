b9a4201a02a215bdbb12d2eb4a92e05e8d685e0f
AnushaPadii <anushapadi142@gmail.com>
2025-01-30 21:44:44 +0530

    Add files via upload

---
+++ Lowercase.java
@@ -0,0 +1,8 @@
+public class Lowercase {
+    public static void main(String[] args) {
+        String str="Hello World";
+        String lowerstrstr=str.toLowerCase();
+        System.out.println("Lowercase:"+lowerstrstr);
+    }
+    
+}
